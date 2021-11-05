ed67804ecabf5933166056e66374273592d41537
AbhishekSharma162000 <abhisheksharmaiit16@gmail.com>
2021-11-05 11:24:45 +0530

    Rendering Stream Data with StreamBuilder

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -7,24 +7,27 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (ctx, index) => Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('This Works!'),
-              )),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('chats/xOzm1CABoljWKbWD8Bf9/messages')
+              .snapshots(),
+          builder: (ctx, streamSnapshot) {
+            if (streamSnapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final documents = streamSnapshot.data!.docs;
+            return ListView.builder(
+                itemCount: documents.length,
+                itemBuilder: (ctx, index) => Container(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(documents[index]['text']),
+                    ));
+          }),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/xOzm1CABoljWKbWD8Bf9/messages')
-              .snapshots()
-              .listen((data) {
-            data.docs.forEach((document) {
-              print(document['text']);
-            });
-          });
-        },
+        onPressed: () {},
       ),
     );
   }
