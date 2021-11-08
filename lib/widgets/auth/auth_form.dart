ab98bdbfa7e289d9f66a78b8d6649d808ed98bc4
AbhishekSharma162000 <abhisheksharmaiit16@gmail.com>
2021-11-08 10:55:06 +0530

    Connecting Auth Form & Auth Screen

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
-  const AuthForm({Key? key}) : super(key: key);
+  const AuthForm({Key? key, required this.submitFn}) : super(key: key);
+
+  final void Function(
+    String email,
+    String password,
+    String userName,
+    bool isLogin,
+  ) submitFn;
 
   @override
   _AuthFormState createState() => _AuthFormState();
@@ -20,9 +27,7 @@ class _AuthFormState extends State<AuthForm> {
     FocusScope.of(context).unfocus();
     if (isValid) {
       _formkey.currentState!.save();
-      print(_userEmail);
-      print(_userName);
-      print(_userPassword);
+      widget.submitFn(_userEmail, _userPassword, _userName, _isLogin);
     }
   }
 
