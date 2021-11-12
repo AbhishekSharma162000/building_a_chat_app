0da8b71589d6fcd25e14aec398b5c9121454e21f
AbhishekSharma162000 <abhisheksharmaiit16@gmail.com>
2021-11-12 12:33:05 +0530

    Listening to Chat Messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:building_a_chat_app/widgets/auth/chat/messages.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -39,24 +40,16 @@ class ChatScreen extends StatelessWidget {
           )
         ],
       ),
-      body: StreamBuilder<QuerySnapshot>(
-          stream: FirebaseFirestore.instance
-              .collection('chats/xOzm1CABoljWKbWD8Bf9/messages')
-              .snapshots(),
-          builder: (ctx, streamSnapshot) {
-            if (streamSnapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            final documents = streamSnapshot.data!.docs;
-            return ListView.builder(
-                itemCount: documents.length,
-                itemBuilder: (ctx, index) => Container(
-                      padding: const EdgeInsets.all(8),
-                      child: Text(documents[index]['text']),
-                    ));
-          }),
+      // ignore: avoid_unnecessary_containers
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Messages(),
+            )
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
