f106c7925184538a18194d1e7f6a9ab3a8767502
AbhishekSharma162000 <abhisheksharmaiit16@gmail.com>
2021-11-17 14:34:41 +0530

    Adding an Image Picker

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -1,3 +1,4 @@
+import 'package:building_a_chat_app/widgets/pickers/user_image_picker.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
@@ -43,6 +44,7 @@ class _AuthFormState extends State<AuthForm> {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
+                if (!_isLogin) UserImagePicker(),
                 TextFormField(
                   key: const ValueKey('email'),
                   validator: (value) {
